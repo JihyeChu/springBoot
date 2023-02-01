c2204a681bda67d5750c5b03cc654f1bdc932311
ChuJH <09strawberry@naver.com>
2023-02-01 22:24:08 +0900

    0627

---
+++ springBoot/src/main/java/com/tjoeun/springBoot/dto/ArticleForm.java
@@ -0,0 +1,39 @@
+package com.tjoeun.springBoot.dto;
+
+public class ArticleForm {
+
+	private String title;
+	private String content;
+	
+	public ArticleForm() { }
+
+	public ArticleForm(String title, String content) {
+		this.title = title;
+		this.content = content;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	@Override
+	public String toString() {
+		return "ArticleForm [title=" + title + ", content=" + content + "]";
+	}
+	
+	
+	
+	
+}
